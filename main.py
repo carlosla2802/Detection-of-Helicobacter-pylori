17bc1c4a000ba5c346405f09614aa1a59df15389
abelespinc <abel.espin@connecthink.eu>
2023-11-10 12:55:05 +0100

    update

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from prep_data import prep_data_main
 from autoencoder import Autoencoder
 from train import train_autoencoder, load_model, make_losses_plot
 from eval import evaluate_model, visualize_reconstructions, save_model
-<<<<<<< HEAD
 from classify import detect_h_pylori_all_validation, calculate_metrics, evaluate_reconstructions
-
-=======
->>>>>>> 0cd59632ab448c93f82b3b2a010cc72f6d0781aa
 
 import torch.nn as nn
 import torch
@@ -18,7 +14,6 @@ def main(use_pretrained=False, pretrained_path=None, plot=True):
     # Definir rutas a los archivos de metadatos y directorio de imágenes
     annotated_patches_path = "metadata/window_metadata.csv"
     labeled_patients_path = "metadata/metadata.csv"
-<<<<<<< HEAD
     annotated_images_dir = "AnnotatedPatches"
     labeled_images_dir = "CroppedPatches"
 
@@ -39,7 +34,7 @@ def main(use_pretrained=False, pretrained_path=None, plot=True):
         # Entrenar el modelo
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu"); autoencoder.to(device)
         num_epochs = 10 # Ajustar num epochs
-=======
+
     annotated_images_dir = 'AnnotatedPatches'
 
     # Preparar data
@@ -68,7 +63,6 @@ def main(use_pretrained=False, pretrained_path=None, plot=True):
         # autoencoder.to(device)
         
         num_epochs = 100 # Ajustar num epochs
->>>>>>> 0cd59632ab448c93f82b3b2a010cc72f6d0781aa
         
         history = train_autoencoder(
             autoencoder, criterion, optimizer, non_infected_train_loader, non_infected_val_loader, num_epochs, device
@@ -76,33 +70,10 @@ def main(use_pretrained=False, pretrained_path=None, plot=True):
     
         if plot:
             make_losses_plot(history)
-<<<<<<< HEAD
     
         # Guardar modelo
         date = datetime.now().strftime("%Y%m%d_%H%M%S")
         save_model(autoencoder, f'autoencoder_{date}.pth')
-
-
-    # Evaluar el modelo
-    reconstructions = evaluate_model(autoencoder, non_infected_val_loader, device)
-
-    if plot:
-        # Visualizar rconstrucciones
-        visualize_reconstructions(reconstructions)
-    
-    reconstructed_validation_patches_data_tensor = evaluate_reconstructions(autoencoder, validation_patches_data_tensor, device)
-
-    pred_labels = detect_h_pylori_all_validation(validation_patches_data_tensor, reconstructed_validation_patches_data_tensor)
-    report = calculate_metrics(validation_patches_labels, pred_labels)
-
-    print("Metrics of the classification method: \n", report)
-
-=======
-    
-        # Guardar modelo
-        date = datetime.now().strftime("%Y%m%d_%H%M%S")
-        save_model(autoencoder, f'autoencoder_{date}.pth')
->>>>>>> 0cd59632ab448c93f82b3b2a010cc72f6d0781aa
 
     # -- para MAC --
     # autoencoder.to(device)
@@ -116,8 +87,4 @@ def main(use_pretrained=False, pretrained_path=None, plot=True):
     
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     main(use_pretrained=True, pretrained_path="autoencoder.pth", plot=True)
-=======
-    main(use_pretrained=False, pretrained_path="autoencoder.pth", plot=True)
->>>>>>> 0cd59632ab448c93f82b3b2a010cc72f6d0781aa
